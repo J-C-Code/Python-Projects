dc16c89b16eb5e7e46001cc9d0460e0a4b1bb52e
Jomma52637 <134970850+Jomma52637@users.noreply.github.com>
2024-04-19 14:33:43 -0500

    Update main.py

--- Projects/Basic Storage/main.py
+++ Projects/Basic Storage/main.py
@@ -1,1 +1,26 @@
+import requests
+import json
 
+
+pantryID = TOKEN
+
+pantry = (f'https://getpantry.cloud/apiv1/pantry/{pantryID}/basket/testBasket')
+
+def requestData():
+    data = requests.get(pantry).json()
+    data = json.dumps(data, indent=2)
+    return data
+
+
+Person = json.dumps({
+  "name": "Jomma",
+  "Is Awesome": True,
+  "Age": 22
+})
+headers = {
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", pantry, headers=headers, data=Person)
+
+print(requestData())
